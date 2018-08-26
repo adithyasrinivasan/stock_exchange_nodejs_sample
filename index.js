6e9c5b992d5536034e63b837d14f7ad1bac44db0
Adithya <sharks12@live.in>
2018-08-26 17:28:58 +0200

    Parameter validation

--- index.js
+++ index.js
@@ -9,5 +9,34 @@ server.listen(server_port, () => {
 });
 
 server.get('/', (req, res) => {
-    res.send('Hello World!');
+    let result = {};
+    result.status = false; //defaults to false. Explicitly set true when all conditions pass
+    result.message = [];
+    let query = req.query;
+
+    //check for querystring size
+    if (Object.keys(query).length <= 2){
+       result.message.push(`http://localhost/countrycode=US&Category=Automobile&BaseBid=10`);
+    }  else {
+
+        //now check if the three parameters we require are specified
+        if (!query.countrycode){
+            result.message.push(`Countrycode is missing`);
+        }
+
+        if (!query.Category){
+            result.message.push(`Category is missing`);
+        }
+
+        if (!query.BaseBid){
+            result.message.push(`BaseBid is missing`);
+        }
+
+        if (result.message.length == 0){
+            
+        }
+
+    }
+
+    res.send(result);
 });
